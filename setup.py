5c53a19ab68bfc1dc87891c3e71701efb5637a7f
Lucas <lluc23@hotmail.com>
2022-11-17 21:21:29 +0100

    update version

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/lluc2397/bfet",
-    version="0.1.3",
+    version="0.1.4",
     zip_safe=False,
 )
