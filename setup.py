c216fd57a6a1d9a2fd5ccb8a7c25fb0c8e78103e
Lucas <lluc23@hotmail.com>
2022-12-07 19:34:31 +0100

    Bump version: 0.1.4 → 0.1.5

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/lluc2397/bfet",
-    version="0.1.4",
+    version="0.1.5",
     zip_safe=False,
 )
